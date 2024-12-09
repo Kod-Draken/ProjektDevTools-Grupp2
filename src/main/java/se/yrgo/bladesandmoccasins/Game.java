5bbf057b61efd19cf2feaaf044fcabdb251738c8
Kod-Draken <m.dackhammar@gmail.com>
2024-12-09 11:15:06 +0100

    resolved issue in main caused by commit: f3a3756

--- src/main/java/se/yrgo/bladesandmoccasins/Game.java
+++ src/main/java/se/yrgo/bladesandmoccasins/Game.java
@@ -3,13 +3,31 @@ package se.yrgo.bladesandmoccasins;
 import java.util.Scanner;
 
 public class Game {
+    Player player;
     public void menu() {
         System.out.println("Hello gladiator! Welcome to Blades and Moccasins.");
         System.out.println("You are just about to begin your career in the arena...");
         System.out.println("But before that, I must ask.. What is your name?");
-        try (Scanner scanner = new Scanner(System.in)){
+        try (Scanner scanner = new Scanner(System.in)) {
             String name = scanner.nextLine();
-            Player player = new Player(name);
+
+            System.out.println("Chose a weapon\n1. Gladus\n2. Spear\n3. Greatsword");
+            String weapon = "";
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    weapon = "Gladus";
+                    break;
+                case 2:
+                    weapon = "Spear";
+                    break;
+                case 3:
+                    weapon = "Greatsword";
+                    break;
+                default:
+                    System.out.println("Something went wrong");
+            }
+            player = new Player(name, weapon);
         }
     }
 }
