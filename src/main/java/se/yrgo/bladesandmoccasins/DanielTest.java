b179158b8c96945ca4c551a562c42f86c5dce7fc
root <root@Danielsbrbara>
2024-12-10 15:23:51 +0100

    addar bara för att pulla och pusha

--- src/main/java/se/yrgo/bladesandmoccasins/DanielTest.java
+++ src/main/java/se/yrgo/bladesandmoccasins/DanielTest.java
@@ -2,7 +2,7 @@ package se.yrgo.bladesandmoccasins;
 
 public class DanielTest {
     public static void main(String[] args) {
-        Score score = new Score("Daniel", 10);
+        Score score = new Score("blb", 10);
 
         System.out.println(score.toString());
     }
--- src/main/java/se/yrgo/bladesandmoccasins/Score.java
+++ src/main/java/se/yrgo/bladesandmoccasins/Score.java
@@ -1,9 +1,9 @@
 package se.yrgo.bladesandmoccasins;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Score {
     private String name;
@@ -12,33 +12,41 @@ public class Score {
     public Score(String name, int value){
         this.name = name;
         this.value = value;
-        try {
-            changeScoorBoard();
-        } catch (Exception e) {
-           System.out.println(e.getMessage());
-        }
+        //try {
+            //changeScoorBoard();
+        //} catch (IOException e) {
+          // System.out.println(e.getMessage());
+          // e.printStackTrace();
+        //}
     }
 
     private void changeScoorBoard()throws IOException{
-        try(BufferedReader buff = new BufferedReader(new FileReader("Score.txt"))){
+        try(BufferedReader buff = Files.newBufferedReader(Path.of("src/main/java/se.yrgo.bladesandmoccasins/Score.txt"))){
             String line;
             while ((line=buff.readLine())!= null) {
                 String[] splitLine = line.split(" ");
+
+
+
                 if(name.equals(splitLine[0]) && (value > Integer.parseInt(splitLine[1]))){
-                    try (BufferedWriter writ = new BufferedWriter(new FileWriter("Score.txt"))) {
+                    try (BufferedWriter writ = Files.newBufferedWriter(Path.of("Score.txt"))){
                         writ.append(line);
-                    } catch (Exception e) {
-                        // TODO: handle exception
+                    }
+                    catch (IOException e) {
+                        System.out.println(e.getMessage());
+                        e.printStackTrace();
                     }
                 }
             }
         }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 
-
+    @Override
     public String toString(){
-
-        try(BufferedReader buff = new BufferedReader(new FileReader("Score.txt"))){
+        try(BufferedReader buff = Files.newBufferedReader(Path.of("se.yrgo.bladesandmoccasins/Score.txt"))){
             String line;
             for(int i = 1; i<=10; i++){
                 line = buff.readLine();
@@ -47,7 +55,8 @@ public class Score {
             return " ";
         }
         catch(Exception e){
-            e.getMessage();
+            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
         return "";
     }
